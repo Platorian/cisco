e01488046e2fa19b2b23653eaaada9bf0c10d0be
Platorian <ghost.dcuo@gmail.com>
2024-06-16 10:52:48 +0100

    updated notes

--- git-upload.sh
+++ git-upload.sh
@@ -1,5 +1,19 @@
+#!/bin/bash
+
+# move into correct directory
+cd ~/Documents/github/cis-notes/cisco
+
+# add all new file
 git add .
 sleep 5
+
+# commit new files to repo
 git commit -m "updated notes"
-sleep 5
+sleep 10
+
+# push files to main
 git push origin main
+sleep 10
+
+# print status
+git status
